ef57a2d48cb0c9cb570d53d738cbdc9b4c0ff901
zivp <zivp1010@gmail.com>
2021-12-05 19:37:16 +0200

    add .env

--- src/test/java/com/example/testNg.java
+++ src/test/java/com/example/testNg.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -21,12 +23,28 @@ public class testNg {
 
   @Test(priority = 1)
   public void firstTest() throws Exception {
+
+    try{
     System.out.println("open google URL");
-    driver.get("https://www.google.com");
+    driver.get("https://doxi-sign.com/#/consist");
     driver.manage().window().maximize();
     Thread.sleep(3000);
 
+    //check if the of the realm is "consist"
+   String text =  driver.findElement(By.xpath("//*[@id='kc-header-wrapper']")).getText();
+   Assert.assertEquals("consist",text.toLowerCase());
+   
+   /*login to DOXI 
+   1. insert name 
+   2. insert password
+   */
+    }
+    catch(Exception e)
+    {System.out.println(">> Exception : "+e); }
+    catch(AssertionError e)
+    {  org.testng.Assert.fail(">> AssertionError : "+e); }
   }
+
 
   @AfterClass()
   public void afterClass() throws Exception {
